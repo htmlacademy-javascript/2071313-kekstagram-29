713ae155a7fe508b121f34fc8c26c11f36b83f3a
Keks <keks@htmlacademy.ru>
2023-07-25 18:19:37 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,9 @@
+import { OBJECT_COUNT } from './setup.js';
+import { createPhotos } from './data.js';
+import { createGallery } from './gallery.js';
+import './upload-image.js';
+import './scale.js';
+
+const pictures = createPhotos(OBJECT_COUNT);
+
+createGallery(pictures);
